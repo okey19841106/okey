680e5ff37ab3162b8438eeea65694b88b1ee83a5
okey19841106 <iorisue0@163.com>
2014-09-12 18:13:26 +0800

    a

--- work/okeyLib/File/FileBase.cpp
+++ work/okeyLib/File/FileBase.cpp
@@ -4,5 +4,24 @@
 
 namespace okey
 {
-	
+	bool FileBase::Close()
+	{
+		if (m_pFile)
+		{
+			fclose(m_pFile);
+			m_pFile = NULL;
+		}
+		return true;
+	}
+
+	bool FileBase::IsFileExist(const std::string& sName)
+	{
+		FILE* f = fopen(sName.c_str(), "rb");
+		if(f)  // The file exists.
+		{
+			fclose(f);
+			return true;
+		}
+		return false;
+	}
 }
--- work/okeyLib/File/FileBase.h
+++ work/okeyLib/File/FileBase.h
@@ -10,22 +10,55 @@
 #define __OKEY_FILE_BASE_H__
 
 #include "nocopyable.h"
+#include <string>
 
 namespace okey
 {
 	class FileBase : public nocopyable
 	{
+	
 	public:
+		static bool IsFileExist(const std::string& filename);
+	public:
+		FileBase(const std::string& filename): m_FileName(filename){}
 		FileBase():m_pFile(NULL){}
 		virtual ~FileBase(){}
-		virtual bool Open(const char* filename) = 0;
-		virtual bool ReadLine(char* buffer, int32 len) = 0;
-		virtual bool IsEnd() = 0;
-		virtual bool Close() = 0;
+		
+		std::string GetFileName() const {return m_FileName;}
+		void SetFileName(const std::string& name){m_FileName = name;}
+		bool isFileExist() const{ return IsFileExist(m_FileName);}
+		bool isFileOpen() const {return m_pFile == NULL;}
+		
+		// SEEK_SET, SEEK_CUR, SEEK_END
+		void SetPosition(int64 lPos, int32 nType = SEEK_SET) { fseek(m_pFile, lPos, nType); }
+		int64 GetPosition() const { return (int64)ftell(m_pFile); }
+		
+	public:
+		virtual bool Open(){return false;}
+		virtual bool Open(const std::string& filename){return false;}
+		virtual bool ReadLine(char* buffer, int32 len){return false;}
+		virtual bool IsEnd() { return feof(m_pFile);}
+		virtual bool Close();
+		virtual bool ReadBuffer(void* pBuffer, uint32 nBufferSize){ return false;}
+		virtual bool WriteBufer(const void* pBuffer, uint32 nBuffersize){ return false;}
+		
+	public:
+		template<typename T> 
+		bool Read(T& data)
+		{
+			return ReadBuffer((void*)&data, sizeof(T));
+		}
 
+		template<typename T>
+		bool Write(const T& data)
+		{
+			return WriteBufer((void*)&data, sizeof(T));
+		}
 	protected:
+		std::string m_FileName;
 		FILE* m_pFile;
 	};
+
 }
 
 
